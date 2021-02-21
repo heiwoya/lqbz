c8727462c8cd2e08431179d9dcc6f2b511b110e8
heiwoya <78598058+heiwoya@users.noreply.github.com>
2021-02-21 12:56:52 +0800

    Update fqkk.js

--- fqkk.js
+++ fqkk.js
@@ -64,8 +64,8 @@ const $ = new Env('番茄看看自动阅读');
 let status;
 status = (status = ($.getval("fqkkstatus") || "1") ) > 1 ? `${status}` : ""; // 账号扩展字符
 const fqkkurlArr = [], fqkkhdArr = [],fqkkbodyArr = [],fqkkcount = ''
-//let fqkkurl = $.getdata('fqkkurl')
-//let fqkkhd = $.getdata('fqkkhd')
+let fqkkurl = $.getdata('fqkkurl')
+let fqkkhd = $.getdata('fqkkhd')
 let fqkey = ''
 let fqkkxh = ($.getval('fqkkxh') || '23');  // 此处修改循环次数，默认一百
 let fqtx = ($.getval('fqtx') || '100');  // 此处修改提现金额，0.1元等于10，默认为提现一元，也就是100
