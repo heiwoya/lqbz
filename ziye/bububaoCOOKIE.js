18d57621d21cd348c46a4d27cb661bc23b898867
heiwoya <78598058+heiwoya@users.noreply.github.com>
2021-02-28 11:08:10 +0800

    Add files via upload

---
+++ ziye/bububaoCOOKIE.js
@@ -0,0 +1,12 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let bububaotokenVal = `0284D007302DA16CC2130CB20530415G1612368436`
+
+
+
+let bububaocookie = {
+    bububaotokenVal: bububaotokenVal,
+
+}
+
+module.exports = bububaocookie
